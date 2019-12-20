e5855f028c6639d82cfab640d29065ac32863997
Peter Bromley <peterhbromley@gmail.com>
2019-12-20 15:15:30 -0500

    Gan training files

--- CompleteRun/data_helper.py
+++ CompleteRun/data_helper.py
@@ -47,88 +47,134 @@ def collate_variable_length(batch):
     return [one_hot, component]
 
 
-def load_the_data(filter_type="mean_signal", rc=False):
-    # Read in data
-    if filter_type == "mean_signal":
-        x_train = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_train.npy")
-        x_test = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_test.npy")
-        if rc:
-            x_train_rc = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_train_rc.npy")
-            x_test_rc = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_test_rc.npy")
-            x_train = np.concatenate([x_train, x_train_rc])
-            x_test = np.concatenate([x_test, x_test_rc])
-        y_train = np.zeros(len(x_train))
-        y_test = np.zeros(len(x_test))
-    else:
-        x_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_train.npy")
-        y_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_components_train.npy")
-        x_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_test.npy")
-        y_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_components_test.npy")
-        if rc:
-            x_train_rc = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_train_rc.npy")
-            x_test_rc = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_test_rc.npy")
-            x_train = np.concatenate([x_train, x_train_rc])
-            x_test = np.concatenate([x_test, x_test_rc])
-            y_train = np.concatenate([y_train, y_train])
-            y_test = np.concatenate([y_test, y_test])
+# def load_the_data(filter_type="mean_signal", rc=False):
+    # # Read in data
+    # if filter_type == "mean_signal":
+    #     x_train = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_train.npy")
+    #     x_test = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_test.npy")
+    #     if rc:
+    #         x_train_rc = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_train_rc.npy")
+    #         x_test_rc = np.load("/home/pbromley/generative_dhs/data_numpy/ms_one_hot_test_rc.npy")
+    #         x_train = np.concatenate([x_train, x_train_rc])
+    #         x_test = np.concatenate([x_test, x_test_rc])
+    #     y_train = np.zeros(len(x_train))
+    #     y_test = np.zeros(len(x_test))
+    # else:
+    #     x_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_train.npy")
+    #     y_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_components_train.npy")
+    #     x_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_test.npy")
+    #     y_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_components_test.npy")
+    #     if rc:
+    #         x_train_rc = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_train_rc.npy")
+    #         x_test_rc = np.load("/home/pbromley/generative_dhs/data_numpy/strong_one_hot_test_rc.npy")
+    #         x_train = np.concatenate([x_train, x_train_rc])
+    #         x_test = np.concatenate([x_test, x_test_rc])
+    #         y_train = np.concatenate([y_train, y_train])
+    #         y_test = np.concatenate([y_test, y_test])
+    #
+    # if (filter_type == "z_score"):
+    #     y_train = y_train - 1       # raw component data goes from 1-15, want 0-14
+    #     y_test = y_test - 1         # "       "       "    "    "    "     "    "
 
-    if (filter_type == "z_score"):
-        y_train = y_train - 1       # raw component data goes from 1-15, want 0-14
-        y_test = y_test - 1         # "       "       "    "    "    "     "    "
+X_TRAIN = 'train_seqs'
+X_VALIDATION = 'validation_seqs'
+X_TEST = 'test_seqs'
+Y_TRAIN = 'train_components'
+Y_VALIDATION = 'validation_components'
+Y_TEST = 'test_components'
 
-    return x_train, y_train, x_test, y_test
+X_DSETS = [
+    X_TRAIN,
+    X_VALIDATION,
+    X_TEST,
+]
+
+Y_DSETS = [
+    Y_TRAIN,
+    Y_VALIDATION,
+    Y_TEST,
+]
+
+def load_the_data():
+    data = {
+        which : np.load('data/{}.npy'.format(which))
+        for which in X_DSETS + Y_DSETS
+    }
+
+    return data
 
 
-def load_nmf_and_sig_data():
-    nmf_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_nmfs_train.npy")
-    nmf_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_nmfs_test.npy")
-    sig_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_sig_train.npy")
-    sig_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_sig_test.npy")
-    return nmf_train, nmf_test, sig_train, sig_test
+# def load_nmf_and_sig_data():
+#     nmf_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_nmfs_train.npy")
+#     nmf_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_nmfs_test.npy")
+#     sig_train = np.load("/home/pbromley/generative_dhs/data_numpy/strong_sig_train.npy")
+#     sig_test = np.load("/home/pbromley/generative_dhs/data_numpy/strong_sig_test.npy")
+#     return nmf_train, nmf_test, sig_train, sig_test
 
 
-def get_the_dataloaders(batch_size, binary_class=None, weighted_sample=False, one_dim=False, data_type='mean_signal', rc=False):
+def get_the_dataloaders(batch_size, weighted_sample=False):
 
-    x_train, y_train, x_test, y_test = load_the_data(filter_type=data_type, rc=rc)
-    if one_dim:
-        x_train = x_train.transpose(0, 2, 1)
-        x_test = x_test.transpose(0, 2, 1)
-    else:
-        x_train = x_train.reshape(-1, 1, 100, 4)
-        x_test = x_test.reshape(-1, 1, 100, 4)
-    if binary_class is not None:
-        y_train = (y_train == binary_class).astype(int)
-        y_test = (y_test == binary_class).astype(int)
-    class_dist_train = np.bincount(y_train.astype(int))
-    class_dist_test = np.bincount(y_test.astype(int))
+    data = load_the_data()
+    dataloaders = {}
+    for x_label, y_label in zip(X_DSETS, Y_DSETS):
+        x_dset = data[x_label].reshape(-1, 1, 100, 4)
+        y_dset = data[y_label]
+        class_dist = np.bincount(y_dset.astype(int))
+        dhs_dataset = DHSSequencesDataset(x_dset, y_dset)
+        dhs_dataloader = DataLoader(dataset=dhs_dataset,
+                                    batch_size=batch_size,
+                                    shuffle=True)
 
-    dhs_dataset_train = DHSSequencesDataset(x_train, y_train)
-    dhs_dataset_test = DHSSequencesDataset(x_test, y_test)
+        if weighted_sample:
+            class_weights = np.sum(class_dist) / class_dist
+            weights = [class_weights[int(comp)] for _, comp in dhs_dataset]
+            sampler = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
+            dhs_dataloader = DataLoader(dataset=dhs_dataset,
+                                        batch_size=batch_size,
+                                        sampler=sampler)
 
-    ### Sample balanced class distribution
-    class_weights = np.sum(class_dist_train) / class_dist_train    # calculate inverse probs
-    weights = [class_weights[int(comp)] for _, comp in dhs_dataset_train]    # assign weight for every sample
-    sampler_train = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))  # create sampler
+        dataloaders[x_label] = dhs_dataloader
 
-    class_weights = np.sum(class_dist_test) / class_dist_test
-    weights = [class_weights[int(comp)] for _, comp in dhs_dataset_test]
-    sampler_test = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
-
-    ###### For Uniform Length ######
-    if weighted_sample:
-        dhs_dataloader_train = DataLoader(dataset=dhs_dataset_train,
-                                batch_size=batch_size,
-                                sampler=sampler_train)
-        dhs_dataloader_test = DataLoader(dataset=dhs_dataset_test,
-                                batch_size=batch_size,
-                                shuffle=True)
-    else:
-        dhs_dataloader_train = DataLoader(dataset=dhs_dataset_train,
-                                batch_size=batch_size,
-                                shuffle=True)
-        dhs_dataloader_test = DataLoader(dataset=dhs_dataset_test,
-                                batch_size=batch_size,
-                                shuffle=True)
+    return dataloaders
+    # if one_dim:
+    #     x_train = x_train.transpose(0, 2, 1)
+    #     x_test = x_test.transpose(0, 2, 1)
+    # else:
+    #     x_train = x_train.reshape(-1, 1, 100, 4)
+    #     x_test = x_test.reshape(-1, 1, 100, 4)
+    # if binary_class is not None:
+    #     y_train = (y_train == binary_class).astype(int)
+    #     y_test = (y_test == binary_class).astype(int)
+    # class_dist_train = np.bincount(y_train.astype(int))
+    # class_dist_test = np.bincount(y_test.astype(int))
+    #
+    # dhs_dataset_train = DHSSequencesDataset(x_train, y_train)
+    # dhs_dataset_test = DHSSequencesDataset(x_test, y_test)
+    #
+    # ### Sample balanced class distribution
+    # class_weights = np.sum(class_dist_train) / class_dist_train    # calculate inverse probs
+    # weights = [class_weights[int(comp)] for _, comp in dhs_dataset_train]    # assign weight for every sample
+    # sampler_train = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))  # create sampler
+    #
+    # class_weights = np.sum(class_dist_test) / class_dist_test
+    # weights = [class_weights[int(comp)] for _, comp in dhs_dataset_test]
+    # sampler_test = torch.utils.data.sampler.WeightedRandomSampler(weights, len(weights))
+    #
+    # ###### For Uniform Length ######
+    # if weighted_sample:
+    #     dhs_dataloader_train = DataLoader(dataset=dhs_dataset_train,
+    #                             batch_size=batch_size,
+    #                             sampler=sampler_train)
+    #     dhs_dataloader_test = DataLoader(dataset=dhs_dataset_test,
+    #                             batch_size=batch_size,
+    #                             shuffle=True)
+    # else:
+    #     dhs_dataloader_train = DataLoader(dataset=dhs_dataset_train,
+    #                             batch_size=batch_size,
+    #                             shuffle=True)
+    #     dhs_dataloader_test = DataLoader(dataset=dhs_dataset_test,
+    #                             batch_size=batch_size,
+    #                             shuffle=True)
 
 
     ###### For Variable Length ######
@@ -136,4 +182,3 @@ def get_the_dataloaders(batch_size, binary_class=None, weighted_sample=False, on
     #                             batch_size=4,
     #                             shuffle=True,
     #                             collate_fn=collate_variable_length)
-    return dhs_dataloader_train, dhs_dataloader_test
