1b02b00f419a3ee02f46b89abe1aed21e68cbe93
Peter Bromley <pbromley@test0.altiusinstitute.org>
2020-02-10 18:23:02 -0800

    Updated classifiers code

--- CompleteRun/classifiers.py
+++ CompleteRun/classifiers.py
@@ -5,10 +5,12 @@ import data_helper
 import utils
 import numpy as np
 from gen_models import resnet_block
-#import matplotlib
-#matplotlib.use('agg')
-#from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('agg')
+from matplotlib import pyplot as plt
 from sklearn.metrics import precision_recall_curve, average_precision_score, confusion_matrix
+
+FIGURES_PATH = "/home/pbromley/generative_dhs/peterbromley/CompleteRun/classifier_figures/"
 
 class hot_dog_not_hot_dog(nn.Module):
     def __init__(self):
@@ -129,22 +131,23 @@ class tmp(nn.Module):
         )
 
     def forward(self, x):
-        h = self.net(x).view(-1, 512)
+        h = self.net(x.view(-1, 4, 100)).view(-1, 512)
         out = self.fc_net(h)
         return out.squeeze()
 
 
 
 class classifier_trainer():
-    def __init__(self, epochs, bs, lr, b1=0.9, b2=0.999, drop=0.0, c=None):
-        self.dataloader, self.test_dataloader = data_helper.get_the_dataloaders(bs, binary_class=c, weighted_sample=True, one_dim=True, data_type='strong', rc=True)
-        self.c = c
-        if self.c is None:
-            self.model = tmp(drop).to("cuda") ##
-            self.criterion = nn.CrossEntropyLoss().to("cuda")
-        else:
-            self.model = resnet_hot_dog_1d().to("cuda")
-            self.criterion = nn.BCELoss().to("cuda")
+    def __init__(self, epochs, bs, lr, b1=0.9, b2=0.999, drop=0.0):
+        dataloaders = data_helper.get_the_dataloaders(bs, weighted_sample=True)
+
+        self.dataloader = dataloaders['train_seqs']
+        self.validation_dataloader = dataloaders['validation_seqs']
+        self.test_dataloader = dataloaders['test_seqs']
+
+        self.model = tmp(drop).to("cuda") ##
+        self.criterion = nn.CrossEntropyLoss().to("cuda")
+
         self.opt = optim.Adam(self.model.parameters(), lr=lr, betas=(b1, b2))
         self.epochs = epochs
         self.train_hist = []
@@ -169,10 +172,7 @@ class classifier_trainer():
             for i, batch in enumerate(self.dataloader):
                 x, y = batch
                 x = x.float().to("cuda")
-                if self.c is not None:
-                    y = y.float().to("cuda")
-                else:
-                    y = y.long().to("cuda")
+                y = y.long().to("cuda")
                 self.opt.zero_grad()
                 pred = self.model(x)
                 loss = self.criterion(pred, y)
@@ -193,10 +193,7 @@ class classifier_trainer():
                     y_true, y_pred, test_loss = self.get_preds()
                     self.train_hist_test.append(test_loss.item())
                     self.train_hist.append(loss.item())
-                    if self.c is not None:
-                        ap = self.precision_recall(y_true, y_pred)
-                    else:
-                        ap = None
+                    ap = None
                     cm = self.confusion(y_true, y_pred)
                     np.set_printoptions(precision=2, linewidth=100, suppress=True)
                     print('Epoch: {0}, Iter: {1}, Loss: {2}, TestLoss: {3}, AP: {4}'.format(
@@ -206,7 +203,7 @@ class classifier_trainer():
                     print(cm)
                     print("")
                     if epoch == self.epochs-1:
-                        np.save("/home/pbromley/generative_dhs/notebooks/cm.npy", cm)
+                        np.save(FIGURES_PATH + "confusion.png", cm)
                     self.model.train(True)
         return self.model
 
@@ -216,18 +213,12 @@ class classifier_trainer():
         plt.plot(x, self.train_hist)
         plt.xlabel('Iterations')
         plt.ylabel('Loss')
-        if self.c is not None:
-            plt.savefig(('/home/pbromley/generative_dhs/loss_plots/aug-%d.png' % self.c))
-        else:
-            plt.savefig(('/home/pbromley/generative_dhs/loss_plots/aug.png'))
+        plt.savefig(FIGURES_PATH + 'loss.png')
         plt.close()
 
     def get_preds(self):
         y_true = np.zeros(len(self.test_dataloader.dataset))
-        if self.c is not None:
-            y_pred = np.zeros(len(self.test_dataloader.dataset))
-        else:
-            y_pred = np.zeros((len(self.test_dataloader.dataset), 16))
+        y_pred = np.zeros((len(self.test_dataloader.dataset), 16))
         self.model.eval()
         loss = 0
         for i, batch in enumerate(self.test_dataloader):
@@ -236,45 +227,39 @@ class classifier_trainer():
             pred = self.model(batch[0].float().to("cuda")).detach()
             y_pred[i*bs:(i+1)*bs] = pred.cpu().numpy()
             
-            if self.c is not None:
-                loss += self.criterion(pred, batch[1].float().to("cuda"))
-            else:
-                loss += self.criterion(pred, batch[1].long().to("cuda"))
+            loss += self.criterion(pred, batch[1].long().to("cuda"))
         loss = loss / i
         return y_true, y_pred, loss
 
-    def precision_recall(self, y_true, y_pred, plot=False):
-        p, r, thresh = precision_recall_curve(y_true, y_pred)
-        ap = average_precision_score(y_true, y_pred)
-        if plot:
-            plt.step(r, p, color=(utils.get_dhs_colors())[self.c], where='post')
-            plt.fill_between(r, p, step='post', color=(utils.get_dhs_colors())[self.c])
-            plt.xlabel("Recall")
-            plt.ylabel("Precision")
-            plt.title("AUPRC for component {0}, Avg. Precision: {1:0.2f}".format(self.c+1, ap))
-            plt.savefig(('/home/pbromley/generative_dhs/auprc/strong-%d.png' % self.c))
-            plt.close()
-        return ap 
+#    def precision_recall(self, y_true, y_pred, plot=False):
+#        p, r, thresh = precision_recall_curve(y_true, y_pred)
+#        ap = average_precision_score(y_true, y_pred)
+#        if plot:
+#            plt.step(r, p, color=(utils.get_dhs_colors())[self.c], where='post')
+#            plt.fill_between(r, p, step='post', color=(utils.get_dhs_colors())[self.c])
+#            plt.xlabel("Recall")
+#            plt.ylabel("Precision")
+#            plt.title("AUPRC for component {0}, Avg. Precision: {1:0.2f}".format(self.c+1, ap))
+#            plt.savefig(('/home/pbromley/generative_dhs/auprc/strong-%d.png' % self.c))
+#            plt.close()
+#        return ap 
 
     def confusion(self, y_true, y_pred):
-        if self.c is not None:
-            cm = confusion_matrix(y_true, (y_pred >= 0.5).astype(int))
-        else:
-            cm = confusion_matrix(y_true, y_pred.argmax(axis=1))
-            cm = np.around(cm/cm.sum(axis=1).reshape(16, 1), 2)
-            fig, ax = plt.subplots(figsize=(10, 10))
-            ax.imshow(cm, cmap='Blues')
-            labelsx = [item.get_text() for item in ax.get_xticklabels()]
-            labelsy = [item.get_text() for item in ax.get_yticklabels()]
-            labelsx[1:-2] = np.arange(1, 17, 2)
-            labelsy[1:-2] = np.arange(1, 17, 2)
-            ax.set_xticklabels(labelsx)
-            ax.set_yticklabels(labelsy)
-            for i in range(16):
-                for j in range(16):
-                    text = ax.text(j, i, cm[i, j], ha="center", va="center", color="orange")
-            plt.savefig(('/home/pbromley/generative_dhs/auprc/confusion.png'))
-            plt.close()
+        cm = confusion_matrix(y_true, y_pred.argmax(axis=1))
+        cm = np.around(cm/cm.sum(axis=1).reshape(16, 1), 2)
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax.imshow(cm, cmap='Blues')
+        labelsx = [item.get_text() for item in ax.get_xticklabels()]
+        labelsy = [item.get_text() for item in ax.get_yticklabels()]
+        labelsx[1:-2] = np.arange(1, 17, 2)
+        labelsy[1:-2] = np.arange(1, 17, 2)
+        ax.set_xticklabels(labelsx)
+        ax.set_yticklabels(labelsy)
+        for i in range(16):
+            for j in range(16):
+                text = ax.text(j, i, cm[i, j], ha="center", va="center", color="orange")
+        plt.savefig(FIGURES_PATH + 'confusion.png')
+        plt.close()
         return cm
 
 
@@ -310,6 +295,6 @@ if __name__ == "__main__":
     #                print(trainer.train_hist_test[-1])
     
     #trainer = classifier_trainer(50, 256, 0.0018, b1=0.9, b2=0.99, drop=0.2, c=None)
-    trainer = classifier_trainer(50, 256, 0.0018, b1=0.9, b2=0.99, drop=0.2, c=None)
+    trainer = classifier_trainer(50, 256, 0.0018, b1=0.9, b2=0.99, drop=0.2)
     model = trainer.train()
     #torch.save(model.state_dict(), "/home/pbromley/generative_dhs/saved_models/classifiers/aug.pth")
