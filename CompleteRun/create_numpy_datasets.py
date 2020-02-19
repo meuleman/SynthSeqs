c343c781381e75b42ea3ac881701f677ba2e8cb9
Peter Bromley <peterhbromley@gmail.com>
2020-02-18 22:53:52 -0500

    Generate classifier datasets

--- CompleteRun/create_numpy_datasets.py
+++ CompleteRun/create_numpy_datasets.py
@@ -68,6 +68,15 @@ DHS_ANNOTATIONS_DF = load_dhs_annotations(PATH_TO_DHS_MASTERLIST,
 
 NMF_LOADINGS_DF = load_nmf_loadings(PATH_TO_NMF_LOADINGS)
 
+COMPONENT_COLUMNS = [
+    'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
+    'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16',
+]
+
+COMPONENT_COLUMNS_MAP = {
+    c: i for c, i in zip(COMPONENT_COLUMNS, range(16))
+}
+
 
 def get_sequence(chrom, start, end):
     return REFERENCE_GENOME_DICT[chrom][start:end]
@@ -84,7 +93,7 @@ def get_sequence_bounds(row, half_length):
         r = row.summit + half_length + excess
     elif after < half_length:
         excess = half_length - after
-        l = row.summit - half_length - excess 
+        l = row.summit - half_length - excess
         r = row.end
     else:
         l = row.summit - half_length
@@ -99,7 +108,8 @@ def bad_nucleotides(seq):
             return True
     return False
 
-# Get sequences (simultaneously trim long sequences to 200)
+
+# Get sequences (simultaneously trim long sequences to 100)
 def get_sequences_and_trim(df, length):
     print("Getting {} sequences...".format(len(df)))
 
@@ -123,6 +133,25 @@ def get_sequences_and_trim(df, length):
     return seqs, df
 
 
+def get_sequence_strength_cutoffs(df, num_sequences):
+    component = df[COMPONENT_COLUMNS].idxmax(axis=1)
+    component_val = df[COMPONENT_COLUMNS].max(axis=1)
+    nmf_sum = df[COMPONENT_COLUMNS].sum(axis=1)
+
+    df['component'] = component
+    df['component_val'] = component_val
+    df['nmf_sum'] = nmf_sum
+
+    df['proportion'] = df.component_val / df.nmf_sum
+
+    strongest = df.groupby('component')['proportion'].nlargest(num_sequences)
+
+    return {
+        c: strongest[c].min() for c in COMPONENT_COLUMNS
+    }
+
+
+
 # Some constants (hardcoded for now)
 LENGTH = 100
 MEAN_SIGNAL_THRESHOLD = 0.5
@@ -140,21 +169,83 @@ nmf_vectors = full_df.loc[:, 'C1':'C16'].values.astype(float)
 
 components = nmf_vectors.argmax(axis=1)
 
-test_mask = full_df.seqname == 'chr1'
-validation_mask = full_df.seqname == 'chr2'
+dset_labels = ['test', 'validation', 'train']
+
+masks = {}
+masks['test'] = (full_df.seqname == 'chr1')
+masks['validation'] = (full_df.seqname == 'chr2')
+masks['train'] = ~(test_mask | validation_mask)
+
 
 idxs = {
-    'test' : np.where(test_mask)[0],
-    'validation' : np.where(validation_mask)[0],
-    'train' : np.where(~(test_mask | validation_mask))[0],
+    label: np.where(masks[label])[0]
+    for label in dset_labels
 }
 
+dfs = {
+    label: full_df[masks[label]]
+    for label in dset_labels
+}
 
-print('Saving sequences to {}'.format(DATA_DIR))
-for dset in ['test', 'validation', 'train']:
-    idx = idxs[dset]
-    print('{0} set: {1} sequences'.format(dset, len(idx)))
-    np.save('data/{}_seqs.npy'.format(dset), one_hot_seqs[idx])
-    np.save('data/{}_components.npy'.format(dset), components[idx])
+num_seqs_small = {
+    'test': 1000,
+    'validation': 1000,
+    'train': 10000,
+}
 
-full_df.to_csv(DATA_DIR + 'seq_ref.csv')
+num_seqs_large = {
+    'test': 2000,
+    'validation': 2000,
+    'train': 20000,
+}
+
+cutoffs_small = {
+    label: get_sequence_strength_cutoffs(df, num_seqs_small[label])
+    for label, df in dfs.items()
+}
+
+cutoffs_large = {
+    label: get_sequence_strength_cutoffs(df, num_seqs_large[label])
+    for label, df in dfs.items()
+}
+
+def create_full_mask(nmf_loadings, cutoffs, mask):
+    buffer = np.zeros(len(nmf_loadings), dtype=bool)
+    for i in range(len(buffer)):
+        if mask[i]:
+            loading = nmf_loadings[i]
+            c = loading.argmax()
+            proportion = loading.max() / loading.sum()
+            buffer[i] = (proportion > cutoffs[c])
+    return buffer
+
+full_masks_small = {
+    label: create_full_mask(nmf_loadings, cutoffs_small, masks[label])
+    for label in dset_labels
+}
+
+full_masks_large = {
+    label: create_full_mask(nmf_loadings, cutoffs_large, masks[label])
+    for label in dset_labels
+}
+
+# print('Saving sequences to {}'.format(DATA_DIR))
+# for dset in dset_labels:
+#     idx = idxs[dset]
+#     print('{0} set: {1} sequences'.format(dset, len(idx)))
+#     np.save('data/{}_seqs.npy'.format(dset), one_hot_seqs[idx])
+#     np.save('data/{}_components.npy'.format(dset), components[idx])
+
+# full_df.to_csv(DATA_DIR + 'seq_ref.csv')
+
+for label in dset_labels:
+    idx = full_masks_small[label]
+    print('{0} set: {1} sequences'.format(label, len(idx)))
+    np.save('data/{}_seqs_classifier_small.npy'.format(label), one_hot_seqs[idx])
+    np.save('data/{}_components_classifier_small.npy'.format(label), components[idx])
+
+for label in dset_labels:
+    idx = full_masks_large[label]
+    print('{0} set: {1} sequences'.format(label, len(idx)))
+    np.save('data/{}_seqs_classifier_large.npy'.format(label), one_hot_seqs[idx])
+    np.save('data/{}_components_classifier_large.npy'.format(label), components[idx])
