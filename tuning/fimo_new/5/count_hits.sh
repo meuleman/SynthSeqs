6effcdc7c27fdfa4be59b4bf88f174e0b987276b
peterhbromley <peterhbromley@gmail.com>
2020-06-12 11:52:56 -0700

    FILE: More FIMO results.

--- tuning/fimo_new/5/count_hits.sh
+++ tuning/fimo_new/5/count_hits.sh
@@ -5,7 +5,7 @@ rm hits.txt
 touch hits.txt
 for iteration in *.fasta;
 do
-    fimo --bgfile --uniform-- --thresh 1e-5 ../../../memes/placenta/GCM1_GCM_1.meme $iteration
+    fimo --bgfile --uniform-- --thresh 1e-5 ../../../memes/epithelial_kidney/HNF1B_homeodomain_1.meme $iteration
     hits="$(awk -F '\t' '{print $3}' fimo_out/fimo.txt | sort | uniq -c | tail -n +2 | wc -l)"
     echo -e "$iteration\t$hits" >> hits.txt
 done
