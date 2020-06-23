d921cc94dcb867d07ba58f6605b7915f6c4b71f5
peterhbromley <peterhbromley@gmail.com>
2020-06-23 15:04:55 -0700

    FILE: Real data FIMO hit percentages.

---
+++ figures/fimo_hits_real_data/train/1/count_hits.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+motif=$1
+rm hits.txt 
+touch hits.txt
+for iteration in *.fasta;
+do
+    fimo --bgfile --uniform-- --thresh 1e-5 /home/pbromley/projects/synth_seqs/memes/fibroblast1/RUNX3_RUNX_4.meme $iteration
+    hits="$(awk -F '\t' '{print $3}' fimo_out/fimo.txt | sort | uniq -c | tail -n +2 | wc -l)"
+    echo -e "$iteration\t$hits" >> hits.txt
+done
