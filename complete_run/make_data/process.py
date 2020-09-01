bd573144d8c9e964e14b6b36664b0c3be1edee3e
peterhbromley <peterhbromley@gmail.com>
2020-09-01 14:09:17 -0700

    ENH: Rerun with length 200 sequences

--- complete_run/make_data/process.py
+++ complete_run/make_data/process.py
@@ -127,8 +127,6 @@ class DataManager:
         seq_filename = data_filename(label, SEQUENCES, GENERATOR)
         comp_filename = data_filename(label, COMPONENTS, GENERATOR)
         
-        one_hots = one_hots[:, :, [0, 2, 3, 1]]
-
         print(f'Writing generator {label} data.')
         np.save(self.output_path + seq_filename, one_hots)
         np.save(self.output_path + comp_filename, components)
@@ -136,8 +134,6 @@ class DataManager:
     def _write_classifier_data(self, label, one_hots, components, mask):
         seq_filename = data_filename(label, SEQUENCES, CLASSIFIER)
         comp_filename = data_filename(label, COMPONENTS, CLASSIFIER)
-
-        one_hots = one_hots[:, :, [0, 2, 3, 1]]
 
         print(f'Writing classifier {label} data.')
         np.save(self.output_path + seq_filename, one_hots[mask])
