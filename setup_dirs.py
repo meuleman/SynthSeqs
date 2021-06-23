ba108b536b82aa55a974db05be79dea19e2435d9
Peter Bromley <peterhbromley@gmail.com>
2021-06-23 18:08:58 -0400

    Set up dirs correctly for new tuner

--- setup_dirs.py
+++ setup_dirs.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-from generator.constants import NZ
 
 from utils.constants import (
     OUTPUT_DIR,
@@ -11,13 +10,6 @@ from utils.constants import (
     VECTOR_DIR,
     VECTOR_FILE,
 )
-
-from optimize.vectors import TuningVectors
-
-
-def initialize_fixed_vectors(num_vectors, len_vectors, path, seed=None):
-    tv = TuningVectors()
-    tv.save_fixed(num_vectors, len_vectors, path, seed=seed)
 
 
 def init_dirs(output_dir):
@@ -30,22 +22,17 @@ def init_dirs(output_dir):
     if not os.path.exists(output_dir + TUNING_DIR):
         os.makedirs(output_dir + TUNING_DIR)
 
-    vector_path = output_dir + VECTOR_DIR
-    if not os.path.exists(vector_path):
-        os.makedirs(vector_path)
-        initialize_fixed_vectors(100000, NZ, vector_path + VECTOR_FILE)
-
 
 def setup_tuning_dir(output_dir, name):
     dir_name = output_dir + TUNING_DIR + name
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
-    for c in range(16):
+    for c in range(1, 17):
         comp_dir_name = dir_name + f'{c}/'
         if not os.path.exists(comp_dir_name):
             os.makedirs(comp_dir_name)
-        for label in ['loss/', 'softmax/', 'seed/', 'skew/']:
+        for label in ['sequences/', 'performance/']:
             if not os.path.exists(comp_dir_name + label):
                 os.makedirs(comp_dir_name + label)
 
